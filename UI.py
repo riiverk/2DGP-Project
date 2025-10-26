876dcb4fdb563f55c2af443af082f76faa675ba9
riiverk <106905463+riiverk@users.noreply.github.com>
2025-10-26 18:48:35 +0900

    Background 클래스 생성

--- UI.py
+++ UI.py
@@ -3,3 +3,7 @@ from pico2d import load_image
 class HealthBar:
     def __init__(self):
         self.image = load_image('health.png')
+
+class Background:
+    def __init__(self):
+        self.image = load_image('match_background.png')
