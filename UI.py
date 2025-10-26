6f3106e5364b5861b2389b68488ae8383bb1e66b
riiverk <106905463+riiverk@users.noreply.github.com>
2025-10-26 19:04:46 +0900

    각 UI 클래스의 draw 함수 완성 및 world에 추가, 출력

--- UI.py
+++ UI.py
@@ -1,13 +1,35 @@
 from pico2d import load_image
 
 class HealthBar:
-    def __init__(self):
+    def __init__(self, x):
         self.image = load_image('health.png')
+        self.x = x
+        self.y = 750
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        pass
 
 class Background:
     def __init__(self):
         self.image = load_image('match_background.png')
 
+    def draw(self):
+        self.image.draw(0,0)
+
+    def update(self):
+        pass
+
 class Gauge:
-    def __init__(self):
+    def __init__(self, x):
         self.image = load_image('gauge.png')
+        self.x = x
+        self.y = 10
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        pass
--- main.py
+++ main.py
@@ -1,14 +1,39 @@
+from _pyrepl.commands import clear_screen
+
 from pico2d import *
 from UI import *
 
 def reset_world():
-    pass
+    global world
+
+    world = []
+
+    background = Background()
+    world.append(background)
+
+    health = []
+    p1_health = HealthBar(0)
+    p2_health = HealthBar(450)
+    health.append(p1_health)
+    health.append(p2_health)
+    world.append(health)
+
+    gauge = []
+    p1_gauge = Gauge(0)
+    p2_gauge = Gauge(650)
+    gauge.append(p1_gauge)
+    gauge.append(p2_gauge)
+    world.append(gauge)
 
 def update_world():
-    pass
+    for o in world:
+        o.update()
 
 def render_world():
-    pass
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
 
 def handle_events():
     pass
