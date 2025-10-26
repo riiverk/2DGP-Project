547cdc4fde4f79426e2b7f83f35ca33c13b1c09e
riiverk <106905463+riiverk@users.noreply.github.com>
2025-10-26 18:49:16 +0900

    Gauge 클래스 생성

--- UI.py
+++ UI.py
@@ -7,3 +7,7 @@ class HealthBar:
 class Background:
     def __init__(self):
         self.image = load_image('match_background.png')
+
+class Gauge:
+    def __init__(self):
+        self.image = load_image('gauge.png')
