4fcda07ca8fcc5e1342792c438aff74d2289f5bb
riiverk <264926w@gmail.com>
2025-11-12 21:58:34 +0900

    DIO 화면 출력

--- player.py
+++ player.py
@@ -15,7 +15,15 @@ class JoJo:
         pass
 
 class DIO:
+    def __init__(self):
+        self.x, self.y = 300, 400
+        self.frame = 0
+        self.face_dir = 0
+        self.image = load_image('DIO.png')
+        self.image_h = 9061
+
     def draw(self):
-        pass
+        self.image.clip_draw(int(self.frame) * 70 + 15, self.image_h - 150, 70, 130, self.x, self.y, 210, 390)
+
     def update(self):
         pass
