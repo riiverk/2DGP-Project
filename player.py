f42b48170ae30521d1ca682ec9b504e49c101c57
riiverk <264926w@gmail.com>
2025-11-12 21:48:50 +0900

    죠죠 화면 출력

--- player.py
+++ player.py
@@ -2,13 +2,14 @@ from pico2d import load_image
 
 class JoJo:
     def __init__(self):
-        self.x, self.y = 50, 70
+        self.x, self.y = 1300, 400
         self.frame = 0
         self.face_dir = 1
         self.image = load_image('JoJo.png')
+        self.image_h = 4800
 
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 100, 100, 100, 100, self.x, self.y)
+        self.image.clip_draw(int(self.frame) * 70 + 2, self.image_h - 145, 70, 120, self.x, self.y, 210, 360)
 
     def update(self):
         pass
