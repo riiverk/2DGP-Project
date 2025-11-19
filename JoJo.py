ca3fdfa188b72addda0344c415fe15beeab0e989
riiverk <264926w@gmail.com>
2025-11-19 20:20:20 +0900

    JoJo IDLE, RUN 추가

--- JoJo.py
+++ JoJo.py
@@ -1,24 +1,102 @@
-from pico2d import load_image
+from pico2d import load_image, get_time
+from sdl2 import SDL_KEYDOWN, SDL_KEYUP, SDLK_RIGHT, SDLK_LEFT
+
+from state_machine import StateMachine
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+
+class Idle:
+    def __init__(self, jojo):
+        self.jojo = jojo
+
+    def enter(self, e):
+        self.jojo.dir = 0
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        self.jojo.frame = (self.jojo.frame + 1) % 8
+
+    def draw(self):
+        self.jojo.image.clip_draw(
+            int(self.jojo.frame) * 70 + 2,
+            self.jojo.image_h - 145,
+            70, 120,
+            self.jojo.x, self.jojo.y,
+            210, 360
+        )
+
+
+class Run:
+    def __init__(self, jojo):
+        self.jojo = jojo
+
+    def enter(self, e):
+        if right_down(e) or left_up(e):
+            self.jojo.dir = 1
+            self.jojo.face_dir = 1
+        elif left_down(e) or right_up(e):
+            self.jojo.dir = -1
+            self.jojo.face_dir = -1
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        self.jojo.frame = (self.jojo.frame + 1) % 8
+        self.jojo.x += self.jojo.dir * self.jojo.speed
+
+    def draw(self):
+        self.jojo.image.clip_draw(
+            int(self.jojo.frame) * 70 + 2,
+            self.jojo.image_h - 145,
+            70, 120,
+            self.jojo.x, self.jojo.y,
+            210, 360
+        )
+
 
 class JoJo:
     def __init__(self):
         self.x, self.y = 1300, 400
         self.frame = 0
         self.face_dir = -1
+        self.dir = 0
         self.image = load_image('JoJo.png')
         self.image_h = 4800
-        self.speed = 3
+        self.speed = 1
 
-    def draw(self):
-        self.image.clip_draw(int(self.frame) * 70 + 2, self.image_h - 145, 70, 120, self.x, self.y, 210, 360)
+        self.IDLE = Idle(self)
+        self.RUN = Run(self)
+        self.state_machine = StateMachine(
+            self.IDLE,
+            {
+                self.IDLE: {right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
+                self.RUN: {right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE}
+            }
+        )
 
     def update(self):
-        pass
+        self.state_machine.update()
 
-    def move_left(self):
-        self.x -= self.speed
-        self.face_dir = -1
+    def handle_event(self, event):
+        self.state_machine.handle_state_event(('INPUT', event))
 
-    def move_right(self):
-        self.x += self.speed
-        self.face_dir = 1
+    def draw(self):
+        self.state_machine.draw()
--- play_mode.py
+++ play_mode.py
@@ -41,13 +41,10 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                game_framework.quit()
-            elif event.key == SDLK_LEFT:
-                jojo.move_left()
-            elif event.key == SDLK_RIGHT:
-                jojo.move_right()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            jojo.handle_event(event)
 
 def update():
     game_world.update()
